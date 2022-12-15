4abd98cf4743dc45e01afba402deeb6df69f75f9
Tri Kusumawardana <trikusumawardana6@gmail.com>
2022-12-15 17:24:53 +0700

    fix name

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
 const url = "https://trikusumawardana-restfulapi.netlify.app/" // TODO: replace this
-const studentName = "TRI KUSUMAWARDANA" // TODO: replace this
+const studentName = "Tri Kusumawardana" // TODO: replace this
 const studentId = "FE2683205" // TODO: replace this
 
 export { url, studentName, studentId }
