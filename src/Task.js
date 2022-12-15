0158d153da40be997392e9c87563c0cb3e50bf72
Tri Kusumawardana <trikusumawardana6@gmail.com>
2022-12-15 17:15:56 +0700

    sudah commit semuanya

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "TRI KUSUMAWARDANA" // TODO: replace this
+const studentId = "FE2683205" // TODO: replace this
+
+export { url, studentName, studentId }
