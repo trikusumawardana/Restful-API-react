54b1b5d122f55ba410e14b1940529926f748b983
Tri Kusumawardana <trikusumawardana6@gmail.com>
2022-12-15 17:21:14 +0700

    add_url

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://trikusumawardana-restfulapi.netlify.app/" // TODO: replace this
 const studentName = "TRI KUSUMAWARDANA" // TODO: replace this
 const studentId = "FE2683205" // TODO: replace this
 
